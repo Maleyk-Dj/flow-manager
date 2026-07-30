437afa1228117264df46462b3d5694a8f85d682d
Maleyk <m9375774872@gmail.com>
2026-07-30 17:23:44 +0300

    fix: устранены все замечания ревьювера

--- flow-manager/src/test/java/com/maleyk/flow_manager/FlowManagerApplicationTests.java
+++ flow-manager/src/test/java/com/maleyk/flow_manager/FlowManagerApplicationTests.java
@@ -4,15 +4,24 @@ import com.maleyk.flow_manager.controller.FileController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalManagementPort;
 import org.springframework.context.ApplicationContext;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FlowManagerApplicationTests extends BaseIntegrationTest {
 
     @Autowired
     private ApplicationContext context;
+
+    @LocalManagementPort
+    private int managementPort;
 
     @Test
     void contextLoads() {
@@ -20,4 +29,16 @@ class FlowManagerApplicationTests extends BaseIntegrationTest {
         assertThat(context.getBean(FileController.class)).isNotNull();
     }
 
+    @Test
+    void checkHealth() throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:" + managementPort + "/actuator/health"))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertThat(response.statusCode()).isBetween(200, 299);
+    }
 }
